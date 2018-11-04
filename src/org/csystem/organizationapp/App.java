047ff984bf5f06390764165a7cc49b86677adafc
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2018-11-04 12:49:01 +0300

    Database test olarak veri ekleyelim. (Hatalar : Postgre driver libraries olarak eklenmeli, insert into tablename unutulmamalı)

---
+++ src/org/csystem/organizationapp/App.java
@@ -0,0 +1,37 @@
+package org.csystem.organizationapp;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+public class App {
+    //Veritabanına bağlanma testi
+    public static void main(String[] args)
+    {
+        //minimal olarak connection URL, User, Password lazım, gerisini driver manager halledecek
+        String URL = "jdbc:postgresql://localhost:5432/organizationdb";
+        String USER = "postgres";
+        String PASSWORD = "12345";
+
+        String sqlCmd = "insert into participiants (name, email, will_attend, join_time) values (?, ?, ?, ?)";
+
+        try (Connection con = DriverManager.getConnection(URL,USER, PASSWORD);
+             PreparedStatement stmt = con.prepareStatement(sqlCmd))
+        {
+            stmt.setString(1, "Tugberk");
+            stmt.setString(2, "mehmettugberkcelik@gmail.com");
+            stmt.setBoolean(3, true);
+            stmt.setTimestamp(4, Timestamp.valueOf(LocalDateTime.now()));
+
+            stmt.executeUpdate();
+        }
+        catch (Throwable ex) {
+            ex.printStackTrace();
+        }
+
+
+    }
+}
