7ab08cb81140ab0acf246cec52b21b9efb2ca83d
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2018-11-04 13:10:18 +0300

    Enum ile singleton kalıbı aynı şey

--- src/org/csystem/organizationapp/App.java
+++ src/org/csystem/organizationapp/App.java
@@ -8,32 +8,31 @@ import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 public class App {
-    //Veritabanına bağlanma testi
+    //enum
     public static void main(String[] args)
     {
-        //minimal olarak connection URL, User, Password lazım, gerisini driver manager halledecek
-        String URL = "jdbc:postgresql://localhost:5432/organizationdb";
-        String USER = "postgres";
-        String PASSWORD = "12345";
+        A a = A.INSTANCE;
+        A b = A.INSTANCE;
 
-        String sqlCmd = "insert into participiants (name, email, will_attend, join_time) values (?, ?, ?, ?)";
-
-        try (Connection con = DriverManager.getConnection(URL,USER, PASSWORD);
-             PreparedStatement stmt = con.prepareStatement(sqlCmd))
-        {
-            stmt.setString(1, "sumeyye");
-            stmt.setString(2, "sumeyye@gmail.com");
-            stmt.setBoolean(3, true);
-            stmt.setTimestamp(4, Timestamp.valueOf(LocalDateTime.now()));
-
-            stmt.execute();
+        if (a == b) {
+            System.out.println("aynı adres");
         }
-        catch (Throwable ex) {
-            ex.printStackTrace();
-        }
-
-
-
+        
+        Util u2 = Util.util;
 
     }
+}
+
+enum A {
+    //default constructor çağrılır INSTANCE İLE INSTANCE() aynı
+    INSTANCE;
+
+
+
+}
+
+class Util {
+    public static final Util util = new Util();
+
+    private Util() {}
 }
